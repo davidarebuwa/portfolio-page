d1e63aa0275cd4818a258c51a9482b2350cefbc8
David Arebuwa <davidarebuwa.gmail.com>
2021-08-01 15:07:33 +0100

    project data updated againish

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -29,8 +29,8 @@ export default function Projects() {
           </p>
           </div>
           <div class="px-6 pt-4 pb-2">
-          {project.skills.map((skill.name) => (
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{skill}</span>
+          {project.skills.map((skill) => (
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{skill.name}</span>
           ))}
           </div>
           </a>
