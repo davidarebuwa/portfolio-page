3b5b41500629f44cc0abfc26a6166e9ddfb6246d
David Arebuwa <davidarebuwa.gmail.com>
2021-08-02 13:27:20 +0100

    updated contact details

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -45,8 +45,8 @@ function handleSubmit(e) {
                     ADDRESS
                   </h2>
                   <p className="mt-1">
-                    97 Warren St. <br />
-                    New York, NY 10007
+                    91 Branston St. <br />
+                    Birmingham, B186BF
                   </p>
                 </div>
                 <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -54,7 +54,7 @@ function handleSubmit(e) {
                     EMAIL
                   </h2>
                   <a className="text-indigo-400 leading-relaxed">
-                    reedbarger@email.com
+                    davidarebuwa@gmail.com
                   </a>
                   <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                     PHONE
@@ -69,11 +69,10 @@ function handleSubmit(e) {
               onSubmit={handleSubmit}
               className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
               <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-                Hire Me
+                Let's Chat!
               </h2>
               <p className="leading-relaxed mb-5">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-                suscipit officia aspernatur veritatis. Asperiores, aliquid?
+                Be it about web, mobile or system development, Always happy to discuss and solve problems
               </p>
               <div className="relative mb-4">
                 <label htmlFor="name" className="leading-7 text-sm text-gray-400">
